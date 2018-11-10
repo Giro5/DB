19f338cf0e66dd7eb06ecd005baeb5d9498fa3bf
Nikita <39918253+Giro5@users.noreply.github.com>
2018-11-10 05:16:24 +0500

    Add files via upload

--- DB/Form1.Designer.cs
+++ DB/Form1.Designer.cs
@@ -29,37 +29,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Tables");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Queries");
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Forms");
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Reports");
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Tables");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Queries");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Forms");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Reports");
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.buttonDelete = new System.Windows.Forms.Button();
-            this.buttonUpdate = new System.Windows.Forms.Button();
-            this.buttonInsert = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -81,10 +76,8 @@
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newToolStripMenuItem,
             this.openToolStripMenuItem,
             this.saveToolStripMenuItem,
-            this.saveAsToolStripMenuItem,
             this.closeToolStripMenuItem,
             this.toolStripSeparator1,
             this.exitToolStripMenuItem});
@@ -92,58 +85,55 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // newToolStripMenuItem
-            // 
-            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
-            this.newToolStripMenuItem.Text = "New";
-            this.newToolStripMenuItem.Click += new System.EventHandler(this.newMenuItem_Click);
-            // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open...";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveMenuItem_Click);
-            // 
-            // saveAsToolStripMenuItem
-            // 
-            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
-            this.saveAsToolStripMenuItem.Text = "Save As...";
-            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsMenuItem_Click);
             // 
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.closeToolStripMenuItem.Text = "Close";
             this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(120, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
             // 
             // helpToolStripMenuItem
             // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem});
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Text = "View";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
             // 
             // statusStrip1
             // 
@@ -165,67 +155,22 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.groupBox1.Controls.Add(this.buttonDelete);
-            this.groupBox1.Controls.Add(this.buttonUpdate);
-            this.groupBox1.Controls.Add(this.buttonInsert);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.ForeColor = System.Drawing.Color.White;
             this.groupBox1.Location = new System.Drawing.Point(0, 24);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(0);
-            this.groupBox1.Size = new System.Drawing.Size(1200, 77);
+            this.groupBox1.Size = new System.Drawing.Size(1200, 61);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Menu";
-            // 
-            // buttonDelete
-            // 
-            this.buttonDelete.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.buttonDelete.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
-            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonDelete.ForeColor = System.Drawing.Color.Black;
-            this.buttonDelete.Location = new System.Drawing.Point(192, 16);
-            this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(86, 55);
-            this.buttonDelete.TabIndex = 3;
-            this.buttonDelete.Text = "Delete";
-            this.buttonDelete.UseVisualStyleBackColor = false;
-            // 
-            // buttonUpdate
-            // 
-            this.buttonUpdate.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.buttonUpdate.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
-            this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonUpdate.ForeColor = System.Drawing.Color.Black;
-            this.buttonUpdate.Location = new System.Drawing.Point(100, 16);
-            this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.Size = new System.Drawing.Size(86, 55);
-            this.buttonUpdate.TabIndex = 2;
-            this.buttonUpdate.Text = "Update";
-            this.buttonUpdate.UseVisualStyleBackColor = false;
-            // 
-            // buttonInsert
-            // 
-            this.buttonInsert.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.buttonInsert.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
-            this.buttonInsert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonInsert.ForeColor = System.Drawing.Color.Black;
-            this.buttonInsert.Location = new System.Drawing.Point(8, 16);
-            this.buttonInsert.Name = "buttonInsert";
-            this.buttonInsert.Size = new System.Drawing.Size(86, 55);
-            this.buttonInsert.TabIndex = 1;
-            this.buttonInsert.Text = "Insert";
-            this.buttonInsert.UseVisualStyleBackColor = false;
-            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            this.groupBox1.Visible = false;
             // 
             // splitContainer1
             // 
             this.splitContainer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 101);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 85);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -237,7 +182,7 @@
             // 
             this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlDark;
             this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
-            this.splitContainer1.Size = new System.Drawing.Size(1200, 629);
+            this.splitContainer1.Size = new System.Drawing.Size(1200, 645);
             this.splitContainer1.SplitterDistance = 214;
             this.splitContainer1.TabIndex = 3;
             // 
@@ -250,35 +195,32 @@
             this.treeView1.FullRowSelect = true;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
-            treeNode5.Name = "Root0";
-            treeNode5.Text = "Tables";
-            treeNode6.Name = "Root1";
-            treeNode6.Text = "Queries";
-            treeNode7.Name = "Root2";
-            treeNode7.Text = "Forms";
-            treeNode8.Name = "Root3";
-            treeNode8.Text = "Reports";
+            treeNode1.Name = "Root0";
+            treeNode1.Text = "Tables";
+            treeNode2.Name = "Root1";
+            treeNode2.Text = "Queries";
+            treeNode3.Name = "Root2";
+            treeNode3.Text = "Forms";
+            treeNode4.Name = "Root3";
+            treeNode4.Text = "Reports";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode5,
-            treeNode6,
-            treeNode7,
-            treeNode8});
-            this.treeView1.Size = new System.Drawing.Size(214, 629);
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4});
+            this.treeView1.Size = new System.Drawing.Size(214, 645);
             this.treeView1.TabIndex = 0;
             this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_MouseDoubleClick);
             // 
             // tabControl1
             // 
+            this.tabControl1.ContextMenuStrip = this.contextMenuStrip1;
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(982, 629);
+            this.tabControl1.Size = new System.Drawing.Size(982, 645);
             this.tabControl1.TabIndex = 1;
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // contextMenuStrip1
             // 
@@ -308,6 +250,10 @@
             this.toolStripMenuItem3.Text = "Close All";
             this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -324,13 +270,11 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DBEditor";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -345,13 +289,11 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.SplitContainer splitContainer1;
@@ -364,9 +306,7 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
-        private System.Windows.Forms.Button buttonDelete;
-        private System.Windows.Forms.Button buttonUpdate;
-        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
     }
 }
 
--- DB/Form1.cs
+++ DB/Form1.cs
@@ -21,7 +21,7 @@ namespace DB
         private OleDbDataAdapter[] adapters = new OleDbDataAdapter[0];
         private DataGridView[] dataGrids = new DataGridView[0];
         private TabPage[] pages = new TabPage[0];
-
+        private OleDbCommandBuilder[] cb = new OleDbCommandBuilder[0];
 
         public Form1()
         {
@@ -34,11 +34,6 @@ namespace DB
             closeToolStripMenuItem.PerformClick();
         }
 
-        private void newMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void openMenuItem_Click(object sender, EventArgs e)
         {
             openFileDialog1.FileName = "";
@@ -47,13 +42,14 @@ namespace DB
             {
                 closeToolStripMenuItem.PerformClick();
                 string path = openFileDialog1.FileName, file = openFileDialog1.SafeFileName;
-                Text = file + " : Databse- " + path + $" (File format: .{file.Split('.')[1]}) - {NameTitle}";
-                connect.ConnectionString = $@"Provider=Microsoft.Jet.OLEDB.4.0;User ID=Admin;Data Source={path}";
+                Text = file + " : Database- " + path + $" (File format: .{file.Split('.')[1]}) - {NameTitle}";
+                //connect.ConnectionString = $@"Provider=Microsoft.Jet.OLEDB.4.0;User ID=Admin;Data Source={path}";
+                connect.ConnectionString = $@"Provider=Microsoft.ACE.OLEDB.12.0;User ID=Admin;Data Source={path}";//need x64
                 connect.Open();
                 toolStripStatusLabel1.Text = "Reading Database";
                 object[] r = new object[] { null, null, null, "TABLE" };
                 DataTable dataTable = connect.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, r);
-                
+
                 treeView1.Nodes.Clear();
                 TreeNode[] newtree = new TreeNode[dataTable.Rows.Count];
                 ds = new DataSet();
@@ -64,9 +60,12 @@ namespace DB
                     adapters[i] = new OleDbDataAdapter(new OleDbCommand($"SELECT * FROM {newtree[i].Text}", connect));
                     adapters[i].Fill(ds, newtree[i].Text);
                     Array.Resize(ref dataGrids, dataGrids.Length + 1);
-                    dataGrids[i] = new DataGridView{ DataSource = ds.Tables[newtree[i].Text] };
+                    dataGrids[i] = new DataGridView { DataSource = ds.Tables[newtree[i].Text] };
+                    Array.Resize(ref cb, cb.Length + 1);
+                    cb[i] = new OleDbCommandBuilder(adapters[i]);
                 }
                 treeView1.Nodes.AddRange(new TreeNode[] { new TreeNode("Tables", newtree) });
+                treeView1.Nodes[0].Expand();
                 treeView1.Visible = true;
                 connect.Close();
                 toolStripStatusLabel1.Text = "Ready";
@@ -75,12 +74,13 @@ namespace DB
 
         private void saveMenuItem_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void saveAsMenuItem_Click(object sender, EventArgs e)
-        {
-
+            for (int i = 0; i < ds.Tables.Count; i++)
+            {
+                dataGrids[i].EndEdit();
+                dataGrids[i].SelectAll();
+                dataGrids[i].ClearSelection();
+                adapters[i].Update(ds.Tables[i]);
+            }
         }
 
         private void exitMenuItem_Click(object sender, EventArgs e)
@@ -88,41 +88,35 @@ namespace DB
             Close();
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            
-        }
-
         private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             TreeNode sel = treeView1.HitTest(e.Location).Node;
+
             if (sel != null && connect.ConnectionString != "")
             {
                 toolStripStatusLabel1.Text = "Datasheet View";
                 if (!tabControl1.Contains(tabControl1.TabPages[sel.Text]))
                 {
                     tabControl1.TabPages.Add(sel.Text, sel.Text);
-                    //dataGrids[sel.Index] = new DataGridView { DataSource = ds.Tables[sel.Text] };
                     dataGrids[sel.Index].Parent = tabControl1.TabPages[sel.Text];
                     dataGrids[sel.Index].Dock = DockStyle.Fill;
                     dataGrids[sel.Index].BorderStyle = BorderStyle.None;
-                    dataGrids[sel.Index].EditMode = DataGridViewEditMode.EditProgrammatically;
-                    tabControl1.TabPages[sel.Text].ContextMenuStrip = contextMenuStrip1;
                 }
                 tabControl1.SelectTab(sel.Text);
-                //dataGridView1.DataSource = ds.Tables[sel.Text];
             }
         }
 
         private void closeMenuItem_Click(object sender, EventArgs e)
         {
             connect.ConnectionString = "";
-            if(ds != null)
+            if (ds != null)
                 ds.Dispose();
             Array.Resize(ref adapters, 0);
-            //dataGridView1.DataSource = null;
+            Array.Resize(ref dataGrids, 0);
+            tabControl1.TabPages.Clear();
             treeView1.Visible = false;
             Text = NameTitle;
+            toolStripStatusLabel1.Text = "Ready";
         }
 
         private void toolStripMenuItem2_Click(object sender, EventArgs e)
@@ -138,11 +132,13 @@ namespace DB
             toolStripStatusLabel1.Text = "Ready";
         }
 
-        private void buttonInsert_Click(object sender, EventArgs e)
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            FormInsert form = new FormInsert();
-            form.Show();
-
+            for (int i = 0; i < treeView1.Nodes[0].Nodes.Count; i++)
+            {
+                ds.Tables[i].Clear();
+                adapters[i].Fill(ds.Tables[i]);
+            }
         }
     }
 }
